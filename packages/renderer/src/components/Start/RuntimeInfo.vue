91cfe7d2374774c2eceb8fb3fafca745f4dc6a2b
vsTerminus <vsterminus@vsterminus.ca>
2025-05-31 22:50:04 -0500

    Remove the donation request

--- packages/renderer/src/components/Start/RuntimeInfo.vue
+++ packages/renderer/src/components/Start/RuntimeInfo.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="absolute inset-x-0 bottom-0 space-y-4">
-    <div class="alert shadow-lg">
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-             class="stroke-info flex-shrink-0 w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-        </svg>
-        <span>It is running now and enjoy. Please do help me for future development by donating.</span>
-      </div>
-      <div class="flex-none">
-        <button class="btn btn-sm btn-primary" @click="donate">
-          <a href="https://www.paypal.com/paypalme/siconghe?country.x=C2&locale.x=en_US" target="_blank">
-            DONATE A CUP OF COFFEE</a></button>
-      </div>
-    </div>
     <div class="stats shadow bg-neutral-focus flex justify-center">
       <div class="stat">
         <div class="stat-title">Total Loaded Image</div>
