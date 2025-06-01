7af0dcc5bc75734fd69e1e7a637b234243139bc1
vsTerminus <vsterminus@vsterminus.ca>
2025-05-31 23:21:00 -0500

    Rewrite About Page to be more fork-specific

--- packages/renderer/src/components/About/About.vue
+++ packages/renderer/src/components/About/About.vue
@@ -3,42 +3,29 @@
     <div class="hero-content text-center">
       <div class="max-w-3/4">
         <h1 class="text-5xl font-bold">MSFS2020 Map Enhancement</h1>
-        <p class="py-6">By He Sicong</p>
+        <p class="py-6">Original by He Sicong. Linux Fork by vsTerminus.</p>
         <div class="divider"></div>
         <div class="space-x-4">
-          <button class="btn btn-primary btn-sm"><a
-            href="https://zh.flightsim.to/file/19345/msfs-2020-google-map-replacement" target="_blank">HomePage</a>
-          </button>
-          <button class="btn btn-primary btn-sm"><a href="https://github.com/derekhe/msfs2020-map-enhancement"
-                                                    target="_blank">Github
-            Repository</a></button>
-          <button class="btn btn-primary btn-sm"><a href="https://github.com/derekhe/msfs2020-map-enhancement/releases"
-                                                    target="_blank">Old
-            Releases</a></button>
+          <button class="btn btn-primary btn-sm"><a href="https://github.com/vsTerminus/msfs2020-linux-map-enhancement"
+            target="_blank">Github Repository</a></button>
         </div>
         <div class="divider content-center"></div>
-        <p class="text-2xl font-extrabold">Donate (捐赠)</p>
-        <p>Please aware this mod is totally free.</p>
-        <p>请注意该软件完全免费，目前有渠道在出售此软件，请注意识别。</p>
-        <p>Paypal/支付宝/微信</p>
-        <div class="flex space-x-4 justify-center">
-          <button class="btn w-32 h-32 btn-info btn-square" @click="donate">
-            <a href="https://www.paypal.com/paypalme/siconghe?country.x=C2&locale.x=en_US" target="_blank">
-              PAYPAL</a></button>
+        <p class="text-2xl font-extrabold">Linux Fork Changes</p>
+        <br/>
+        <div>
+          <ul>
+            <li>Remove remote log uploading</li>
+            <li>Remove donation links</li>
+            <li>Remove automatic updates</li>
+            <li>Remove embedded nginx (You need to run your own)</li>
+            <li>Remove embedded python (Uses system python now)</li>
+            <li>Update images, branding, package name, etc</li>
+            <li>Use dummy hosts file (You need to edit your own /etc/hosts)</li>
+            <li>etc.</li>
+          </ul>
         </div>
-        <div class="divider"></div>
-        <div class="indicator" v-if="this.statusStore.updateInfo">
-          <div class="indicator-item indicator-bottom">
-            <button class="btn btn-primary"><a href="https://flightsim.to/file/19345/msfs-2020-google-map-replacement"
-                                               target="_blank">Update</a></button>
-          </div>
-          <div class="card border">
-            <div class="card-body">
-              <h2 class="card-title">New version: {{ releaseVersion }}</h2>
-              <div class="w-96 text-left" v-html="releaseNotes"></div>
-            </div>
-          </div>
-        </div>
+        <div class="divider content-center"></div>
+        <p>(I don't know if this works on MSFS 2024, it was built for 2020 and I don't own 2024 yet)</p>
       </div>
     </div>
   </div>
